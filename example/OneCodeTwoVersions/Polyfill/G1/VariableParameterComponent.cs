4698b3bc5ce911348b381c04a2d213a273c31121
karakasa <karakasa@users.noreply.github.com>
2025-04-27 14:42:04 +0800

    added code

---
+++ example/OneCodeTwoVersions/Polyfill/G1/VariableParameterComponent.cs
@@ -0,0 +1,23 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneCodeTwoVersions.Polyfill;
+public abstract partial class VariableParameterComponent<TComp> : ComponentMiddleware<TComp>, IGH_VariableParameterComponent
+    where TComp : VariableParameterComponent<TComp>
+{
+    protected VariableParameterComponent(string name, string nickname, string desc, string category, string subcategory) :
+        base(name, nickname, desc, category, subcategory)
+    {
+    }
+
+    protected VariableParameterComponent(IReader reader) : base(reader) { }
+    public abstract void VariableParameterMaintenance();
+    public abstract bool CanInsertParameter(GH_ParameterSide side, int index);
+    public abstract bool CanRemoveParameter(GH_ParameterSide side, int index);
+    public abstract IGH_Param CreateParameter(GH_ParameterSide side, int index);
+    public abstract bool DestroyParameter(GH_ParameterSide side, int index);
+}
